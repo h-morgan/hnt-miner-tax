caf34f30b04bbe2f72aba1326581e7e7f4797a56
Haley Morgan <haleymorgan3264@gmail.com>
2022-02-26 15:07:49 -0500

    add some support for pdf field id exploration

--- src/taxes/utils.py
+++ src/taxes/utils.py
@@ -1,6 +1,7 @@
 from typing import no_type_check
+#from markupsafe import te
 import pdfrw
-
+import json
 ANNOT_KEY = '/Annots'
 ANNOT_FIELD_KEY = '/T'
 ANNOT_VAL_KEY = '/V'
@@ -14,14 +15,31 @@ PDF_TEXT_APPEARANCE = pdfrw.objects.pdfstring.PdfString.encode('/Courier 8.00 Tf
 def get_keys_in_pdf(pdf_filestring):
     """
     Gets all they key (field id's) of a fillable pdf
+    Just used to setup new schc / pdf forms
     """
+    template_pdf = pdfrw.PdfReader(pdf_filestring)
+    keys = template_pdf.Root.AcroForm.Fields
+
+    # collect a dict with key/ value pairs of key = pdf key, value = ints
+    pdf_test_vals = {}
+    pdf_map = {}
+    increment = 0
     for page in template_pdf.pages:
         annotations = page[ANNOT_KEY]
         for annotation in annotations:
             if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
                 if annotation[ANNOT_FIELD_KEY]:
                     key = annotation[ANNOT_FIELD_KEY][1:-1]
+                    pdf_test_vals[key] = increment
+                    pdf_map[increment] = key
+                    increment += 1
                     print(key)
+
+    # turn pdf map of int: pdf key to json file
+    with open('output/f1040sc_map_ints_2021.json', 'w') as fp:
+        json.dump(pdf_map, fp)
+    return pdf_test_vals
+
 
 
 def fill_pdf(input_pdf_path, output_pdf_path, data_dict):
@@ -79,10 +97,11 @@ def collect_flags(tax_form):
     if tax_form["expenses"]["validator_equipment"]["had_validator"] not in no_values:
         flags["has_validator"] = True
     
-    if tax_form['sold_traded_hnt'] not in no_values:
-        flags["sold_traded_hnt"] = True
-    
-    if tax_form['mined_other_crypto'] not in no_values:
-        flags["mined_other_crypto"] = True
+    if "sold_traded_hnt" in tax_form:
+        if tax_form['sold_traded_hnt'] not in no_values:
+            flags["sold_traded_hnt"] = True
+        
+        if tax_form['mined_other_crypto'] not in no_values:
+            flags["mined_other_crypto"] = True
 
     return flags
